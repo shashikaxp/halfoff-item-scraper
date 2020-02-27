a0833464f5515d37f197a754e44b0f3b57b58ae6
Shashika <Shashika@shashikas-mbp.home>
2020-02-27 22:55:44 +1100

    client side initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Drawer from "./components/drawer";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Drawer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/constants/theme.js
@@ -0,0 +1,21 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#556cd6'
+    },
+    secondary: {
+      main: '#19857b'
+    },
+    error: {
+      main: '#99337b'
+    },
+    background: {
+      default: '#fff'
+    }
+  }
+});
+
+export default theme;
